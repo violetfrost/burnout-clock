00061822556881e380ae7592a2130df34cf237f0
Violet Frost <violetfrostbusiness@gmail.com>
2026-08-08 11:05:39 -0400

    page switching

--- src/assets/js/burnout.js
+++ src/assets/js/burnout.js
@@ -20,6 +20,10 @@ const sessionState = {
 
 const depressedOMeter = document.querySelector('#depressed-o-meter');
 const depressedOMeterValue = document.querySelector('#depressed-o-meter-value');
+const setupPage = document.querySelector('#setup');
+const workPage = document.querySelector('#work');
+const playPage = document.querySelector('#play');
+const setupForm = setupPage.querySelector('form');
 
 // Depresssed-O-Meter UI functionality! 
 
@@ -40,3 +44,41 @@ function updateDepressedOMeter() {
 
 depressedOMeter.addEventListener('input', updateDepressedOMeter);
 updateDepressedOMeter();
+
+// Page switching!
+
+const pages = [setupPage, workPage, playPage];
+function switch_to_page(pageName) {
+    var nextPage;
+
+    switch (pageName) {
+        case 'setup':
+            nextPage = setupPage;
+            break;
+        case 'work':
+            nextPage = workPage;
+            break;
+        case 'play':
+            nextPage = playPage;
+            break;
+        default:
+            nextPage = setupPage;
+    }
+
+    pages.forEach((page) => {
+        page.classList.toggle('d-none', page !== nextPage);
+    });
+}
+
+// Setup Page!
+
+setupForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    if (!setupForm.checkValidity()) {
+        setupForm.reportValidity();
+        return;
+    }
+
+    switch_to_page('work');
+});
