3af0aab9ca81dd19e516b024c1d1e5878d46e690
Violet Frost <violetfrostbusiness@gmail.com>
2026-08-09 13:10:17 -0400

    js cleanup

--- src/assets/js/burnout.js
+++ src/assets/js/burnout.js
@@ -9,17 +9,18 @@ const sessionSegment = {
 const sessionState = {
     sessionName: '',
     recoveryRatio: 0,
+    maxTimeHours: 0,
     segments: [], // meant to be an array of session segments! 
     totalRecoveryTime: 0,
     totalWorkTime: 0
 };
-
 
 // It's not pretty, but we're just going to select various elements
 // here because I really CBA to split everything up into files...
 
 const depressedOMeter = document.querySelector('#depressed-o-meter');
 const depressedOMeterValue = document.querySelector('#depressed-o-meter-value');
+const taskName = document.querySelector('#task-name');
 const setupPage = document.querySelector('#setup');
 const workPage = document.querySelector('#work');
 const playPage = document.querySelector('#play');
@@ -43,31 +44,37 @@ function updateDepressedOMeter() {
 }
 
 depressedOMeter.addEventListener('input', updateDepressedOMeter);
-updateDepressedOMeter();
 
 // Page switching!
 
 const pages = [setupPage, workPage, playPage];
 function switch_to_page(pageName) {
     var nextPage;
+    var loadPage;
 
     switch (pageName) {
         case 'setup':
             nextPage = setupPage;
+            loadPage = setupPageLoaded;
             break;
         case 'work':
             nextPage = workPage;
+            loadPage = workPageLoaded;
             break;
         case 'play':
             nextPage = playPage;
+            loadPage = playPageLoaded;
             break;
         default:
             nextPage = setupPage;
+            loadPage = setupPageLoaded;
     }
 
     pages.forEach((page) => {
         page.classList.toggle('d-none', page !== nextPage);
     });
+
+    loadPage();
 }
 
 // Setup Page!
@@ -80,5 +87,21 @@ setupForm.addEventListener('submit', (event) => {
         return;
     }
 
+    sessionState.sessionName = taskName.value;
+    sessionState.recoveryRatio = depressedOMeterOptions.length - Number(depressedOMeter.value);
+
     switch_to_page('work');
 });
+
+switch_to_page('setup');
+
+function setupPageLoaded() {
+    setupForm.reset();
+    updateDepressedOMeter();
+}
+
+function workPageLoaded() {
+}
+
+function playPageLoaded() {
+}
